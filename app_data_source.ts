3ab3e95cc647f8eb33aef0f5fae92cc5c76cfb3f
Manon Coulombe <manon.coulombe@octo.com>
2025-02-26 15:16:48 +0100

    config database render

--- app_data_source.ts
+++ app_data_source.ts
@@ -6,17 +6,23 @@ import {RepartitionParDefaut} from "./src/entity/RepartitionParDefaut";
 import {RepartitionTransaction} from "./src/entity/RepartitionTransaction";
 import {TypeTransaction} from "./src/entity/TypeTransaction";
 import {TypeDeCompte} from "./src/entity/TypeDeCompte";
+import "dotenv/config";
 
 export const PostgresDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "soleil",
-    database: "equity",
+    host: process.env.HOST,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5432,
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
     synchronize: true,
     logging: false,
     entities: [Compte, Transaction, Participant, RepartitionParDefaut, RepartitionTransaction, TypeTransaction, TypeDeCompte],
     migrations: [],
     subscribers: [],
+    extra: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
 });
