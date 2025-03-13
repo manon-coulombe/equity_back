9eaa8cd708371fa32ee83d100514a74994cc1444
Manon Coulombe <coulombemanon@gmail.com>
2025-03-13 11:17:05 +0100

    save participants when create compte

--- src/controller/compte/ComptePostAction.ts
+++ src/controller/compte/ComptePostAction.ts
@@ -3,32 +3,53 @@ import {Compte} from "../../entity/Compte";
 import {Request, Response} from "express";
 import {TypeDeCompte} from "../../entity/TypeDeCompte";
 import {RepartitionParDefaut} from "../../entity/RepartitionParDefaut";
+import {Participant} from "../../entity/Participant";
 
 export async function comptePostAction(req: Request, res: Response) {
     try {
         const compteRepository = PostgresDataSource.getRepository(Compte);
         const typeRepository = PostgresDataSource.getRepository(TypeDeCompte);
         const repartitionRepository = PostgresDataSource.getRepository(RepartitionParDefaut);
+        const participantRepository = PostgresDataSource.getRepository(Participant);
 
         const compteData = new Compte();
         compteData.nom = req.body.nom;
         compteData.devise = req.body.devise;
 
-        const type = await typeRepository.findOneBy({id: parseInt(req.body.type_id)});
+        const {type_id, repartition_id, participants} = req.body;
+
+
+        const type = await typeRepository.findOneBy({id: parseInt(type_id)});
         if (type) {
             compteData.type = type;
         }
 
-        const repartition = await repartitionRepository.findOneBy({id: parseInt(req.body.repartition_id)});
+        const repartition = await repartitionRepository.findOneBy({id: parseInt(repartition_id)});
         if (repartition) {
             compteData.repartition = repartition;
         }
 
+        const savedCompte = await compteRepository.save(compteData);
 
-        const compte = compteRepository.create(compteData);
-        const savedCompte = await compteRepository.save(compte);
+        if (Array.isArray(participants)) {
+            const participantsEntities = participants.map(({nom, revenus}) => {
+                const participant = new Participant();
+                participant.nom = nom;
+                participant.revenus = revenus;
+                participant.compte = savedCompte;
+                return participant;
+            });
 
-        res.status(201).json(savedCompte);
+            await participantRepository.save(participantsEntities);
+        }
+
+        const compteWithParticipants = await compteRepository.findOne({
+                where: {id: savedCompte.id},
+                relations: ['type', 'repartition', 'participants'],
+            },
+        );
+
+        res.status(201).json(compteWithParticipants);
     } catch (error) {
         console.error("Error creating account", error);
         res.status(500).json({message: "Error creating account"});
